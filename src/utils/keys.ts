3bca1f0ab236b743ab06e7eab66ca30d4d5fb771
Gabriel Matiolli <gabrielmatiollif@gmail.com>
2024-12-13 12:06:32 -0300

    Remoção de arquivos inutilizados

--- src/utils/keys.ts
+++
@@ -1,9 +0,0 @@
-import crypto from "crypto";
-
-// Chave como string hexadecimal
-const hexKey = process.env.HEX_KEY || "456c6f536f6c7574696f6e7340474450";
-// Converte a string hexadecimal em um Buffer
-export const AES_KEY = Buffer.from(hexKey, 'hex'); // Chave deve ter exatamente 32 bytes
-
-// Vetor de inicialização (IV)
-export const AES_IV = crypto.randomBytes(16); // 16 bytes para o IV
